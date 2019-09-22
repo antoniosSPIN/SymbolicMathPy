8914ac9d25193e23eb9d96a0c10d5ecca007c721
Antonios <antonivalmas@gmail.com>
2019-09-22 16:16:32 +0300

    Add model for the test.

--- models.py
+++ models.py
@@ -61,3 +61,89 @@ class HasUserRole(Base):
 
     auth_user = relationship('AuthUser')
     user_role = relationship('UserRole')
+
+
+class Test(Base):
+    """A class representing the test
+    """
+
+    def __init__(self, name, creator_id):
+        self.name = name
+        self.creator_id = creator_id
+    
+    __tablename__ = 'test'
+
+    test_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63), nullable=False, index=True)
+    creator_id = db.Column(db.ForeignKey('auth_user.auth_user_id'), nullable=False, index=True)
+
+    auth_user = relationship('AuthUser')
+
+
+class Problem(Base):
+    """A class representing a problem of a test
+    """
+
+    def __init__(self, problem_statement, test_id):
+        self.problem_statement = problem_statement
+        self.test_id = test_id
+    
+    __tablename__ = 'problem'
+
+    problem_id = db.Column(db.Integer, primary_key=True)
+    problem_statement = db.Column(db.String(511), nullable=False)
+    test_id = db.Column(db.ForeignKey('test.test_id'), nullable=False, index=True)
+
+    test = relationship('Test')
+
+
+class Difficulty(enum.Enum):
+    EASY = 1
+    EASY_MEDIUM = 2
+    MEDIUM = 3
+    MEDIUM_HARD = 4
+    HARD = 5
+
+
+class Question(Base):
+    """A class representing a question of a problem
+    """
+
+    def __init__(self, question, difficulty, marks, answer, solution, problem_id):
+        self.question = question
+        self.difficulty = difficulty
+        self.marks = marks
+        self.answer = answer
+        self.solution = solution
+        self.problem_id = problem_id
+    
+    __tablename__ = 'question'
+
+    question_id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(511), nullable=False)
+    difficulty = db.Column(db.Enum(Difficulty))
+    marks = db.Column(db.Integer(3), nullable=False)
+    answer = db.Column(db.String(255), nullable=False)
+    solution = db.Column(db.String(2047), nullable=False)
+    problem_id = db.Column(db.ForeignKey('problem.problem_id'), nullable=False, index=True)
+
+    problem = relationship('Problem')
+
+
+class HasTakenTest(Base):
+    """A class representing the relationship between the student and the test they have taken
+    """
+
+    def __init__(self, student_id, test_id, score):
+        self.student_id = student_id
+        self.test_id = test_id
+        self.score = score
+    
+    __tablename__ = 'has_taken_test'
+
+    student_id = db.Column(db.ForeignKey('auth_user.auth_user_id'), nullable=False, index=True)
+    test_id = db.Column(db.ForeignKey('test.test_id'), nullable=False, index=True)
+    score = db.Column(db.Integer(3), nullable=False, index=True)
+
+    auth_user = relationship('AuthUser')
+    test = relationship('Test')
